72b61d9a46ae75ef1793496fb4eeb412f4fdd3df
Koyama-Nene <nene.koyama@kusurinomadoguchi.co.jp>
2022-02-08 13:33:04 +0900

    index create

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>商品一覧画面</title>
+</head>
+<body>
+    <h1>商品一覧画面です</h1>
+</body>
+</html>

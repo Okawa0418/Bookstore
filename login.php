62822d4920b70224223372bb344a5aa52798391f
Okawa0418 <makoto.okawa@kusurinomadoguchi.co.jp>
2022-02-09 13:30:32 +0900

    ログイン機能作成

--- login.php
+++ login.php
@@ -1,19 +1,33 @@
 <?php
-    // ファイルの取り込み(データベースへの接続)
-    require_once("database2.php");
+session_start();
+$mail = $_POST['mail_address'];
+$dsn = "mysql:host=localhost; dbname=bookstore; charset=utf8";
+$username = "root";
+$password = "";
+try {
+    $dbh = new PDO($dsn, $username, $password);
+} catch (PDOException $e) {
+    $msg = $e->getMessage();
+}
+
+$sql = "SELECT * FROM user WHERE mail_address = :mail_address";
+$stmt = $dbh->prepare($sql);
+$stmt->bindValue(':mail_address', $mail);
+$stmt->execute();
+$member = $stmt->fetch();
+
     //指定したハッシュがパスワードにマッチしているかチェック
-    if (password_verify($_POST['pass'], $member['pass'])) {
+    if (password_verify($_POST['password'], $member['password'])) {
     //DBのユーザー情報をセッションに保存
-    $_SESSION['id'] = $member['id'];
-    $_SESSION['name'] = $member['name'];
+    $_SESSION['user_id'] = $member['user_id'];
+    $_SESSION['user_name'] = $member['user_name'];
     $msg = 'ログインしました。';
-    $link = '<a href="index.php">ホーム</a>';
+    $link = '<a href="confirm.php">購入画面</a>';
 } else {
     $msg = 'メールアドレスもしくはパスワードが間違っています。';
-    $link = '<a href="login.php">戻る</a>';
+    $link = '<a href="login_form.php">戻る</a>';
 }
 ?>
 
 <h1><?php echo $msg; ?></h1>
 <?php echo $link; ?>
-?>

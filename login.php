af774498a8ac1cfc1e01fde48c67eaba725d4270
Okawa0418 <makoto.okawa@kusurinomadoguchi.co.jp>
2022-02-09 16:56:39 +0900

    bootstrap実装

--- login.php
+++ login.php
@@ -1,3 +1,11 @@
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+ 
+<!-- Bootstrap Javascript(jQuery含む) -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 <?php
 session_start();
 $mail = $_POST['mail_address'];
@@ -21,8 +29,8 @@ $member = $stmt->fetch();
     //DBのユーザー情報をセッションに保存
     $_SESSION['user_id'] = $member['user_id'];
     $_SESSION['user_name'] = $member['user_name'];
-    $msg = 'ログインしました。';
-    $link = '<a href="confirm.php">購入画面</a>';
+    $msg = '<h2>ログインしました。</h2>';
+    $link = '<h1><a href="confirm.php">購入画面</a></h1>';
 } else {
     $msg = 'メールアドレスもしくはパスワードが間違っています。';
     $link = '<a href="login_form.php">戻る</a>';

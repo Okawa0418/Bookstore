6368e19362a78db6ce7eb1216ab29c042c56375b
Takuya0717 <98365806+Takuya0717@users.noreply.github.com>
2022-02-14 17:04:03 +0900

    商品購入画面　新規追加

--- confirm.php
+++ confirm.php
@@ -1,3 +1,46 @@
+<?php
+session_start();
+// 変更id からname
+$product_name = $_SESSION['product']['name'];
+$amount_quantity = $_SESSION['product']['quantity'];
+$price= $_SESSION['product']['price'];
+$total_amount = $_SESSION['product']['total_amount'];
+
+// foreach pr
+foreach ($product_name as $key => $name) {
+    echo '<pre>';
+    echo  $name;
+    echo '<pre>';
+}
+foreach ($amount_quantity as $key => $value) {
+    echo '<pre>';
+    echo  $value;
+    echo '<pre>';
+}
+foreach ($price as $key => $value) {
+    echo '<pre>';
+    echo  $value;
+    echo '<pre>';
+}
+// foreach ($total_amount as $key => $value) {
+//     echo '<pre>';
+//     echo  $value;
+//     echo '<pre>';
+// }
+?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>商品追加</title>
+</head>
+<body>
+    <!--アクションで完了画面へ -->
+<form name ="form1" method="post" action="done.php">
+    <h2>商品を確認後追加ボタンを押して下さい</h2>
+    <input type="submit" value="発注する" name="confirm">
+</form>
+</body>
+</html>
 <!--  -->
 <!-- // session_start();
 // $product_id = $_SESSION['product']['id'];
@@ -55,23 +98,4 @@ if (isset($_SESSION['user_id']) === true) {
     print '<br><br>';
 }
 ?> -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>商品購入</title>
-</head>
-<body>
-商品確認画面
-<h2>商品名</h2>
-<?php foreach ($_POST as $product_id); ?>
-<h2>数量</h2>
-<?php foreach ($_POST as $quantity); ?>
-<h2>合計</h2>
-<?php foreach ( $_POST as $total_amount ); ?>
-<!--アクションで完了画面へ -->
-<form name ="form1" method="post" action="done.php">
- <input type="submit" value="注文を確定する" name="confirm">
-</form>
-</body>
-</html>
 
--- validate.php
+++ validate.php
@@ -1,9 +1,9 @@
 <!-- バリデーション実装 -->
 <?php
-session_start();
-var_dump($_POST);
-exit;
-if(empty($_POST['name'])){
+// session_start();
+// var_dump($_POST);
+// exit;
+if(empty($_POST['value'])){
     // エラーメッセージをセッションに格納
     $_SESSION['msg'] = 'ユーザー名を入力してください。';
     // 登録画面に戻る
@@ -11,22 +11,22 @@ if(empty($_POST['name'])){
     exit;
 }
         // セッション変数にエラーメッセージが格納されていた場合
-    if (isset($_SESSION['msg'])) {
+    // if (isset($_SESSION['msg'])) {
         // 変数へ代入
-        $msg = $_SESSION['msg'];
+        // $msg = $_SESSION['msg'];
         // エラーメッセージのセッション破棄
-        unset($_SESSION['msg']);
-    }
+        // unset($_SESSION['msg']);
+    // }
     // エラーメッセージ
-    $err=[];
+    // $err=[];
     // バリデーション
-    if(!$name=filter_input(INPUT_POST,'name')){
-    $err[]='名前を入力してください。';
-    }
+    // if(!$value=filter_input(INPUT_POST,'name')){
+    // $err[]='名前を入力してください。';
+    // }
     
-    if(!$price=filter_input(INPUT_POST,'price')){
-    $err[]='値段を入力してください';
-    }
+    // if(!$value=filter_input(INPUT_POST,'price')){
+    // $err[]='値段を入力してください';
+    // }
 ?> 
 </body>
 </html>

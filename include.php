62f0a500fe94daecff185dfc24b5c28e6ef3a034
Takuya0717 <98365806+Takuya0717@users.noreply.github.com>
2022-02-14 13:11:07 +0900

    購入確認画面、新規追加

--- include.php
+++ include.php
@@ -72,12 +72,12 @@
     <div style="font-size:14px">新規本を追加してください</div>
     <form method="post" action="index.php">
         名前を記入してください:<br>
-        <input type="text" name="name">
+        <input type="text" name="name" value="">
         <br>
         値段を記入してください:<br>
-        <input type="text" name="price">
+        <input type="text" name="price" value="">
         <!-- <br> -->
-        <input type="submit" name="validate" value="発注">
+        <input type="submit" name="submit" value="発注">
     </form>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>

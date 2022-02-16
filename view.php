e30a23c813f34e6eb7e34f4f4a61576f10934a8c
Koyama-Nene <nene.koyama@kusurinomadoguchi.co.jp>
2022-02-16 13:17:23 +0900

    画像アップロード、アンケートリンク表示
    imagesフォルダー作成
    productテーブルにfile_pathカラム追加
    商品一覧画面にアンケートリンク表示

--- view.php
+++ view.php
@@ -1,39 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Document</title>
-</head>
-<body>
 <?php
-$db_user = 'root';
-$db_pass = 'Rilakkuma1231';
-$db_host = 'localhost';
-$db_name = 'bookstore';
-$db_type = 'mysql';
+require_once('database1.php');
+$database = new Database1;
+$pdo = $database->dbConnect();
+// $db_user = 'root';
+// $db_pass = 'Rilakkuma1231';
+// $db_host = 'localhost';
+// $db_name = 'bookstore';
+// $db_type = 'mysql';
 
-$dsn = "$db_type:host=$db_host;dbname=$db_name;charset=utf8";
+// $dsn = "$db_type:host=$db_host;dbname=$db_name;charset=utf8";
 
-try {
-    $pdo = new PDO($dsn, $db_user, $db_pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-    print '発注中 <br>';
-} catch (PDOException $Exception) {
-    die('エラー:' . $Exception->getMessage());
-}
+// try {
+//     $pdo = new PDO($dsn, $db_user, $db_pass);
+//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+//     print '発注中 <br>';
+// } catch (PDOException $Exception) {
+//     die('エラー:' . $Exception->getMessage());
+// }
 try {
     $pdo->beginTransaction();
-    $sql = 'INSERT INTO product (product_name,price)VALUES(:name,:price)';
-    $stmh = $pdo->prepare($sql);
-    $stmh->bindValue(':name', $_POST['name'], PDO::PARAM_STR);
-    $stmh->bindValue(':price', $_POST['price'], PDO::PARAM_INT);
-    $stmh->execute();
+    $sql = 'INSERT INTO product (product_name, price, file_path, category) VALUES (:name, :price, :file_path, :category)';
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(':name', $_POST['name'], PDO::PARAM_STR);
+    $stmt->bindValue(':price', $_POST['price'], PDO::PARAM_INT);
+    $stmt->bindValue(':file_path', $file_path, PDO::PARAM_STR);
+    $stmt->bindValue(':category', $_POST['category'], PDO::PARAM_INT);
+    $stmt->execute();
     $pdo->commit();
-    print 'データを' . $stmh->rowCount() . '件、挿入しました。<br>';
+    print 'データを' . $stmt->rowCount() . '件、挿入しました。<br>';
 } catch (PDOException $Exception) {
     $pdo->rollBack();
     print 'エラー:' . $Exception->getMessage();
 }
 ?>
-</body>
-</html>

f29df50c29d9fc1d8762ff77962861a9f45ce34e
Okawa0418 <makoto.okawa@kusurinomadoguchi.co.jp>
2022-02-18 14:34:35 +0900

    ログアウト画面からトップページへの遷移実装

--- logout.php
+++ logout.php
@@ -8,22 +8,5 @@ if (ini_get("session.use_cookies")) {
     setcookie(session_name(), '', time() - 42000, '/');
 }
 session_destroy();
+header('Location: index.php');
 ?>
-
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ログアウト画面</title>
-</head>
-<body>
-    <header>
-        <h1>BOOK STORE</h1>
-    </header>
-    <h1>ログアウト完了</h1>
-    <h2>ログアウトしました。</h2>
-    <a href="index.php">トップページ</a>
-</body>
-</html>

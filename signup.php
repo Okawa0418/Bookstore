da38b36811116c0f07aef69790bb6be5ccb4574e
Okawa0418 <makoto.okawa@kusurinomadoguchi.co.jp>
2022-02-24 12:16:37 +0900

    リファクタリング

--- signup.php
+++ signup.php
@@ -1,18 +1,16 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="ja">
  
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
- 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
- 
     <title>ユーザー登録画面</title>
 </head>
  
 <body>
     <header>
-    <a href="index.php"  style="color:inherit;text-decoration: none;"><h1>BOOK STORE</h1></a>
+      <a href="index.php"  style="color:inherit;text-decoration: none;"><h1>BOOK STORE</h1></a>
     </header>
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark mt-3 mb-3">
         <a class="navbar-brand" href="#">会員登録</a>
@@ -23,6 +21,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
 
     <?php
     session_start();
@@ -61,39 +60,34 @@
         unset($_SESSION['msg6']);
     }
     ?>
-    
-<form action="register.php" method="post">
-<div class="container">
+
+<body>
+  <form action="register.php" method="post">
+  <div class="container">
 
     <!-- エラーメッセージの表示 -->
     <?php if (isset($msg)) : ?>
-      <?= $msg; ?>
-      <br>
+      <?= $msg; ?><br>
     <?php endif ; ?>
 
     <?php if (isset($msg2)) : ?>
-      <?= $msg2; ?>
-      <br>
+      <?= $msg2; ?><br>
     <?php endif ; ?>
 
     <?php if (isset($msg3)) : ?>
-      <?= $msg3; ?>
-      <br>
+      <?= $msg3; ?><br>
     <?php endif ; ?>
 
     <?php if (isset($msg4)) : ?>
-      <?= $msg4; ?>
-      <br>
+      <?= $msg4; ?><br>
     <?php endif ; ?>
 
     <?php if (isset($msg5)) : ?>
-      <?= $msg5; ?>
-      <br><br>
+      <?= $msg5; ?><br><br>
     <?php endif ; ?>
 
     <?php if (isset($msg6)) : ?>
-      <?= $msg6; ?>
-      <br><br>
+      <?= $msg6; ?><br><br>
     <?php endif ; ?>
 
     <div>
@@ -115,19 +109,18 @@
     <div>
       <label>パスワード：</label>
       <input type="password" name="password" placeholder="例）abcd1234">
-    </div>
-<br>
+    </div><br>
+
     <div class="row">
         <div class="col-md">
           <input type="submit" class="btn btn-success" value="登録完了">
         </div>
-    </div>
-<br>
-</form>
-<p>既に登録済みの方は<a href="login_form.php">こちら</a></p>
-<form action="index.php">
-    <input type="submit" class="btn btn-secondary" value="トップページ">
-</form>
-</div>
-  </body>
+    </div><br>
+  </form>
+
+  <p>既に登録済みの方は<a href="login_form.php">こちら</a></p>
+  <form action="index.php">
+      <input type="submit" class="btn btn-secondary" value="トップページ">
+  </form>
+</body>
 </html>

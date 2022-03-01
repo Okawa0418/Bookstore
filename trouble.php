af36b26fcc68b0df8bb36e3e0c78eddb20489c5b
Takuya0717 <98365806+Takuya0717@users.noreply.github.com>
2022-03-02 08:57:59 +0900

    質問ボット、商品購入後　商品履歴Link先挿入

--- trouble.php
+++ trouble.php
@@ -77,6 +77,7 @@
                                                     また、最終ご注文品の店頭到着日から30日間は、商品お引き取り確認期間として、「会員情報の変更」ページからの退会手続きは制限させていただいております。
 
                                                     予めご了承ください。</p>
+                                                    解決しない場合大変ご迷惑をお掛けしてます下のお問い合わせリンク先に行きお問い合わせフォームの項目番号1を選択した後要件を記入してください。
                                                 <!-- ※linkで退会へ繋げる -->
                                                 </div>
                                                 <div class="works_modal_close">✖</div>
@@ -94,13 +95,14 @@
             <ul class="list-group list-group-flush">
                         <!-- 文字 -->
                 <h1>2)クレジットカード使用について</h1>
-                <p>ああ</p>
+                <p>クレジットカード決済は現在使用する事はできません</p>
                 <li class="list-group-item">
+                    <a href="customerformadd.php"class="link-primary">お問い合わせフォーム</a>
                     <ul class="works_list">
                         <li class="works_item">
                             <div class="works_image">
                                 <div class="works_modal_open" data-modal-open="modal-2">
-                                    <p>会員登録、ログイン、退会に関する質問</p>
+                                    <p>クレジットカード使用について</p>
                                     <div class="works_image_mask">
                                         <div class="mask_text">クリックで拡大</div>
                                     </div>
@@ -109,11 +111,11 @@
                                             <div class="works_modal_window">
                                                 <div class="works_modal_content">
                                                     <!-- 中身内容変更 -->
-                                                    <h1>基本操作</h1>
-                                                    <img src="list.png" width="700" height="200">
-                                                    <p>1</p>
-                                                    <img src="show.png" width="700" height="200">
-                                                    <p>1</p>
+                                                    <h1>クレジット決済</h1>
+                                                    <h1>クレジット決済の使用は現在行っていません。ご了承ください。
+                                                        本サイトでは商品画面をで商品、数量を選択した後ボタンをクリックするだけで購入できます
+                                                        解決しない場合大変ご迷惑をお掛けしてますが下のお問い合わせリンク先に行きクレジットカードの項目番号2を選択した後要件を記入してください。
+                                                    </h1>
                                                 </div>
                                                 <div class="works_modal_close">✖</div>
                                             </div>
@@ -129,13 +131,14 @@
             <ul class="list-group list-group-flush">
                         <!-- 文字 -->
                 <h1>3）購入履歴について</h1>
-                <p>ああ</p>
+                <p>購入履歴についてのよくある質問</p>
                 <li class="list-group-item">
+                    <a href="pur_history.php"class="link-danger">購入履歴</a>
                     <ul class="works_list">
                         <li class="works_item">
                             <div class="works_image">
                                 <div class="works_modal_open" data-modal-open="modal-3">
-                                    <p>会員登録、ログイン、退会に関する質問</p>
+                                    <p>購入履歴</p>
                                     <div class="works_image_mask">
                                         <div class="mask_text">クリックで拡大</div>
                                     </div>
@@ -144,11 +147,10 @@
                                             <div class="works_modal_window">
                                                 <div class="works_modal_content">
                                                     <!-- 中身内容変更 -->
-                                                    <h1>基本操作</h1>
-                                                    <img src="list.png" width="700" height="200">
-                                                    <p>1</p>
-                                                    <img src="show.png" width="700" height="200">
-                                                    <p>1</p>
+                                                    <h1>購入履歴について</h1>
+                                                    <h1>購入履歴を反映するのに時間がかかる恐れがあります。解決しない場合大変ご迷惑をお掛けしてますが下のお問い合わせリンク先に行きお問い合わせフォームの項目番号3を選択した後エラーを記入してください。
+                                                        なおお手数をおかけしますが返信には時間がかかる場合がございますのでご了承ください。
+                                                    </h1>
                                                 </div>
                                                 <div class="works_modal_close">✖</div>
                                             </div>
@@ -163,13 +165,15 @@
             <ul class="list-group list-group-flush">
                         <!-- 文字 -->
                 <h1>4)入荷日について</h1>
-                <p>ああ</p>
+                <p>ご希望日時の選択以外での入荷日はお客様の現在住所に左右される可能性があります</p>
                 <li class="list-group-item">
+                    <a href="customerformadd.php"class="link-primary">お問い合わせフォーム</a>
+                    <a href="describe.php"class="link-secondary">ご利用説明</a>
                     <ul class="works_list">
                         <li class="works_item">
                             <div class="works_image">
                                 <div class="works_modal_open" data-modal-open="modal-4">
-                                    <p>会員登録、ログイン、退会に関する質問</p>
+                                    <p>入荷日</p>
                                     <div class="works_image_mask">
                                         <div class="mask_text">クリックで拡大</div>
                                     </div>
@@ -178,11 +182,10 @@
                                             <div class="works_modal_window">
                                                 <div class="works_modal_content">
                                                     <!-- 中身内容変更 -->
-                                                    <h1>基本操作</h1>
-                                                    <img src="list.png" width="700" height="200">
-                                                    <p>1</p>
-                                                    <img src="show.png" width="700" height="200">
-                                                    <p>1</p>
+                                                    <h1>入荷日に関しまして万が一商品が一週間以上経過した場合でも届けられていない場合には以下の原因が考えられます
+                                                        1）BOOKSTORE側の出荷間違い2）配達トラブル3)住所登録先の記載間違い   
+                                                        大変申し訳ございません。1.2の場合はBOOKSTOREの出荷担当へお電話いてください 0534-7979-7979 
+                                                    </h1>
                                                 </div>
                                                 <div class="works_modal_close">✖</div>
                                             </div>
@@ -197,13 +200,14 @@
             <ul class="list-group list-group-flush">
                         <!-- 文字 -->
                 <h1>5)利用規約について</h1>
-                <p>ああ</p>
+                <p>BOOKSTORは会員登録無料で提供させていただいています。</p>
                 <li class="list-group-item">
+                        <a href="customerformadd.php"class="link-primary">お問い合わせフォーム</a>
                     <ul class="works_list">
                         <li class="works_item">
                             <div class="works_image">
                                 <div class="works_modal_open" data-modal-open="modal-5">
-                                    <p>会員登録、ログイン、退会に関する質問</p>
+                                    <p>ご利用規約</p>
                                     <div class="works_image_mask">
                                         <div class="mask_text">クリックで拡大</div>
                                     </div>
@@ -212,11 +216,20 @@
                                             <div class="works_modal_window">
                                                 <div class="works_modal_content">
                                                     <!-- 中身内容変更 -->
-                                                    <h1>基本操作</h1>
-                                                    <img src="list.png" width="700" height="200">
-                                                    <p>1</p>
-                                                    <img src="show.png" width="700" height="200">
-                                                    <p>1</p>
+                                                    <h1>ご利用規約事項</h1>
+                                                    <p>本規約は、株式会社くすりの窓口が運営するインターネットサイト「BOOKSTORE」の利用に関し、会員と、BOOKSTORE上で商品またはサービスを会員に提供する目的でBOOKSTORE上に仮想店舗を出店している者（以下「加盟書店」といいます）およびBOOKSTOREとの間に適用されるものとします。
+                                                    入会）
+
+                                                        BOOKSTORの会員になることを希望する個人、法人その他責任者の定めのある団体（以下「入会希望者」といいます）は、本規約の内容を理解し、その内容に拘束されることを承諾したうえで、入会登録画面においてトーハン所定の事項（以下「登録事項」といいます）をすべて届け出るものとします。なお、同一の入会希望者が複数の会員登録を行うことはできません。
+                                                        会員登録手続は、前項の届け出に対するBOOKSTOREの承諾をもって完了するものとします。ただし、BOOKSTOREは、入会希望者が以下に定める事由のいずれかに該当することが判明した場合、入会希望者の入会を認めないことがあります。
+                                                        ① 入会希望者が実在しないこと。
+
+                                                        ② 入会希望者がすでに会員になっている場合。
+
+                                                        ③ 入会希望者が過去に本規約違反等により、会員資格の停止処分を受けたことがあり、または、過去に本規約違反等で会員資格の抹消が行われたことがある場合。
+
+                                                        ④ BOOKSTORに届け出た事項に虚偽、誤記または記入もれがあった場合。
+                                                    </p>
                                                 </div>
                                                 <div class="works_modal_close">✖</div>
                                             </div>

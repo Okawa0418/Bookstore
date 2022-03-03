fa16ce95ada7a300189d7802f206abb0ac993603
Takuya0717 <98365806+Takuya0717@users.noreply.github.com>
2022-03-03 16:56:02 +0900

    会員登録流れ　デザイン

--- login_form.php
+++ login_form.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="login_form.css">
     <title>ログイン画面</title>
 </head>
  
@@ -12,7 +13,6 @@
     <header>
         <a href="index.php"  style="color:inherit;text-decoration: none;"><h1>BOOK STORE</h1></a>
     </header>
-
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark mt-3 mb-3">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav4" aria-controls="navbarNav4" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -21,7 +21,6 @@
         <div class="collapse navbar-collapse">
         </div>
     </nav>    
-
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
@@ -82,6 +81,25 @@
     <form action="index.php">
         <input type="submit" class="btn btn-secondary" value="トップページ">
     </form>
+    </div>
+</body>
+</html>
+<!-- デザイン -->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="px-4 py-5 my-5 text-center">
+        <img class="d-block mx-auto mb-4" src="dublin.jpg"  width="800" height="400">
+        <h1 class="display-5 fw-bold">BOOKSTORE</h1>
+        <div class="col-lg-6 mx-auto">
+            <p class="lead mb-4">looking forward and looking back that's our life</p>
+        </div>
     </div>
 </body>
 </html>
--- register.php
+++ register.php
@@ -6,13 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>登録完了画面</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="register.css">
 </head>
 
 <body>
     <header>
     <a href="index.php"  style="color:inherit;text-decoration: none;"><h1>BOOK STORE</h1></a>
     </header>
-
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
@@ -86,6 +86,30 @@ session_start();
         $stmt->bindValue(':tel', $tel);
         $stmt->execute();
         echo '<h1>会員登録が完了しました</h1>';
-        echo '<a href="login_form.php">ログインページ</a>';
+        echo '<a href="describe.php" class="link-warning">ご利用方法</a>';
+        echo '<form method="post" action="login_form.php">
+        <input type="submit" value="ログインページ" name="login" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">
+        </form>';
     }
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <footer>
+        <div class="px-4 py-5 my-5 text-center">
+            <img class="d-block mx-auto mb-4" src="describetwoguys.jpg"  width="800" height="400">
+            <h1 class="display-5 fw-bold">BOOKSTORE</h1>
+            <div class="col-lg-6 mx-auto">
+                <p class="lead mb-4">BOOKSTOREへようこそ!素敵な本に出会えますように</p>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>

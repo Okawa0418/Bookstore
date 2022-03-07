60a4e112407f4cebd43f1b089a9498de3edd64cd
Takuya0717 <98365806+Takuya0717@users.noreply.github.com>
2022-03-07 12:07:39 +0900

    カレンダー作成

---
+++ schedule.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>schedule</title>
+<style>
+<body>
+body {
+    background-color: #999;
+}
+.container {
+    display: flex;
+    background-color: white;
+    justify-content: flex-end;
+    align-items: start;
+    height: 1000px;
+    flex-wrap: wrap;
+    align-content;flex-start;
+    }
+    .box {
+        color: white;
+        padding: 5px;
+        width: 100px;
+        height: 100px;
+    } 
+    box:first-child{
+        height: 500px;
+    } 
+    </style> 
+</head>
+
+<body>
+    <div class="container bg-white d-flex align-content-stretch flex-wrap">
+        <div class="box bg-secondary" style="width: 100px">1</div>        
+        <div class="box bg-primary" style="width: 100px">2</div>
+        <div class="box bg-secondary" style="width: 100px">3</div>
+        <div class="box bg-primary" style="width: 100px">4</div>               
+        <div class="box bg-secondary" style="width: 100px">5</div>
+        <div class="box bg-primary" style="width: 100px">6</div>
+        <div class="box bg-secondary" style="width: 100px">7</div>
+        <div class="box bg-primary" style="width: 100px">8</div>
+        <div class="box bg-secondary" style="width: 100px">9</div>
+        <div class="box bg-primary" style="width: 100px">10</div>
+        <div class="box bg-secondary" style="width: 100px">11</div>
+        <div class="box bg-primary" style="width: 100px">12<p>新規商品入庫日</p></div>
+        <div class="box bg-secondary" style="width: 100px">13</div>
+        <div class="box bg-primary" style="width: 100px">14</div>
+        <div class="box bg-secondary" style="width: 100px">15</div>
+        <div class="box bg-primary" style="width: 100px">16</div>
+        <div class="box bg-secondary" style="width: 100px">17</div>
+        <div class="box bg-primary" style="width: 100px">18</div>
+        <div class="box bg-secondary" style="width: 100px">19<p>新規商品入庫日</p></div>
+        <div class="box bg-primary" style="width: 100px">20</div>
+        <div class="box bg-secondary" style="width: 100px">21<p>システム保守臨時休暇</p></div>
+        <div class="box bg-primary" style="width: 100px">22</div>
+        <div class="box bg-secondary" style="width: 100px">23</div>
+        <div class="box bg-primary" style="width: 100px">24</div>
+        <div class="box bg-secondary" style="width: 100px">25</div>
+        <div class="box bg-primary" style="width: 100px">26<p>新規商品入庫日</p></div>
+        <div class="box bg-secondary" style="width: 100px">27</div>
+        <div class="box bg-primary" style="width: 100px">28</div>
+        <div class="box bg-secondary" style="width: 100px">29</div>
+        <div class="box bg-primary" style="width: 100px">30</div>
+        <div class="box bg-secondary" style="width: 100px">31</div>
+    </div>
+
+</body>
+</html>

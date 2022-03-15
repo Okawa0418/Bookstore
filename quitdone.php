f78a7f76000d96d08ecb8af563aba169c31c53e1
Takuya0717 <98365806+Takuya0717@users.noreply.github.com>
2022-03-15 09:41:28 +0900

    文字統一

--- quitdone.php
+++ quitdone.php
@@ -85,7 +85,7 @@
 <div class="container-fluid">    
     <h4>またのご利用をお待ちしております。</h4>
     <form action="index.php">
-        <button type="submit" class="btn btn-secondary">商品一覧へ</button>
+        <button type="submit" class="btn btn-secondary">商品一覧</button>
     </form>
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
